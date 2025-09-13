34ad098a8981185d3c7261f77bbe01364aa25a6b
JP <jota.tiger.dev@gmail.com>
2025-09-13 03:49:15 -0300

    fix start:watch script, add dynamic port to app server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.send("Hello World 3!");
 });
 
-app.listen(3333, () => {
-    console.log("Server is running on port 3333");
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
