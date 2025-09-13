b2af2c238d85f7d9b300bcf72c38387c0a20d247
JP <jota.tiger.dev@gmail.com>
2025-09-13 03:19:04 -0300

    install @types/express, create base app api

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.listen(3333, () => {
+    console.log("Server is running on port 3333");
+});
